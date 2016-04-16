57630a049073f7ad69d7d8037c57f1a25d3518de
Vadim Nikitin <asm.i8086@gmail.com>
2016-04-16 18:04:32 +0700

    Added handling of circular dependencies with some error checking

--- deps.go
+++ deps.go
@@ -1,64 +1,109 @@
 /**
 * deps - dependency management utility for Linux executables
-*/
+ */
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "os"
-    "os/exec"
-    "strings"
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 type dependency struct {
-    Name string
-    Path string
-    Addr string
+	Name string
+	Path string
+	Addr string
 }
 
 func getDeps(lddOutput string) []dependency {
-    lines := strings.Split(lddOutput, "\n")
-    depsCount := len(lines) - 1
-    deps := make([]dependency, depsCount)
-        
-    for i, l := range lines {
-        dep := strings.Split(l, " ")
-        
-        switch {
-        case len(dep) > 3:
-            deps[i].Name = strings.TrimSpace(dep[0])
-            deps[i].Path = strings.TrimSpace(dep[2])
-            deps[i].Addr = strings.TrimSpace(dep[3])
-        case len(dep) > 1:
-            idx := strings.LastIndex(dep[0], "/")
-            deps[i].Name = strings.TrimSpace(string(dep[0][idx + 1:]))
-            deps[i].Path = strings.TrimSpace(dep[0])
-            deps[i].Addr = strings.TrimSpace(dep[1])
-        }
-    }
+	lines := strings.Split(lddOutput, "\n")
+	depsCount := len(lines) - 1
+	deps := make([]dependency, depsCount)
 
-    return deps
+	for i, l := range lines {
+		dep := strings.Split(l, " ")
+
+		// ldd has a little weird output:
+		// 1) name => path (addr)
+		//    libc.so.6 => /lib/i386-linux-gnu/libc.so.6 (0xb753d000)
+		// 2) path (addr)
+		//    /lib/ld-linux.so.2 (0xb7746000)
+		// 3) and a strange (this is VDSO provided by kernel, no need to copy it):
+		//     linux-gate.so.1 =>  (0xb7745000)
+		// so handle different types of it
+
+		switch {
+		case len(dep) > 3: // case 1
+			deps[i].Name = strings.TrimSpace(dep[0])
+			deps[i].Path = strings.TrimSpace(dep[2])
+			deps[i].Addr = strings.TrimSpace(dep[3])
+		case len(dep) > 1: // case 2
+			idx := strings.LastIndex(dep[0], "/")
+			deps[i].Name = strings.TrimSpace(string(dep[0][idx+1:]))
+			deps[i].Path = strings.TrimSpace(dep[0])
+			deps[i].Addr = strings.TrimSpace(dep[1])
+		}
+	}
+
+	return deps
 }
 
-func walkDeps(what, where string) {
-    ldd := exec.Command("ldd", what)
-    var lddOut bytes.Buffer
-    ldd.Stdout = &lddOut
-    ldd.Run()
-    
-    deps := getDeps(lddOut.String())  
-    for _, d := range deps {
-        fmt.Printf("-Dependency:\n  name: %s\n  path: %s\n  addr: %s\n", d.Name, d.Path, d.Addr)
-        cp := exec.Command("cp", "--parent", d.Path, where)
-        cp.Run()
-        walkDeps(d.Path, where)
-    }
+var visitedDeps map[string]bool
+
+func walkDeps(path, dir string) error {
+	// Do not walk among visited paths,
+	// or else the recursion will be infinite
+	if _, ok := visitedDeps[path]; ok {
+		fmt.Printf("! %s already copied\n", path)
+		return nil
+	}
+
+	// Check this path as visited
+	visitedDeps[path] = true
+
+	// Get dependency list by executing ldd
+	ldd := exec.Command("ldd", path)
+	var lddOut bytes.Buffer
+	ldd.Stdout = &lddOut
+	ldd.Run()
+
+	// Parse ldd's output
+	deps := getDeps(lddOut.String())
+
+	// Walk through new dependencies recursively to gather them all
+	for _, d := range deps {
+		fmt.Printf("-Dependency:\n  name: %s\n  path: %s\n  addr: %s\n", d.Name, d.Path, d.Addr)
+
+		cp := exec.Command("cp", "--parent", d.Path, dir)
+		cp.Run()
+
+		if err := walkDeps(d.Path, dir); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func main() {
-    what := os.Args[1]
-    where := os.Args[2]
-    os.Mkdir(where, 0777)
-    walkDeps(what, where)
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: deps EXECUTABLE PATH\nGather dependencies of an executable")
+		return
+	}
+
+	what := os.Args[1]
+	whereToCopy := os.Args[2]
+
+	if err := os.Mkdir(whereToCopy, 0777); err != nil {
+		panic("Could not create output directory - " + err.Error())
+	}
+
+	visitedDeps = make(map[string]bool)
+	if err := walkDeps(what, whereToCopy); err != nil {
+		panic("An error has occured while gathering dependencies - " + err.Error())
+	}
+
+	fmt.Println("Finished!")
 }
